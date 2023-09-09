5ce3344064224b4f89ff3145f74bbb785f091bb0
Jed Patterson <jedpatterson123@gmail.com>
2023-09-09 19:37:27 +0100

    Postgres

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,9 @@ import redis
 from decouple import config
 from urllib.parse import urlparse
 import sentry_sdk
+from sentry_sdk import capture_exception
 from sentry_sdk.integrations.flask import FlaskIntegration
+import psycopg2
 
 sentry_sdk.init(
     dsn="https://49a1a41b45c948689562808601f5d48b@o1072423.ingest.sentry.io/4505851487715328",
@@ -29,46 +31,53 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-KV_URL = config('KV_URL')
-KV_REST_API_URL = config('KV_REST_API_URL')
-KV_REST_API_TOKEN = config('KV_REST_API_TOKEN')
-KV_REST_API_READ_ONLY_TOKEN = config('KV_REST_API_READ_ONLY_TOKEN')
+POSTGRES_URL = config('POSTGRES_URL')
+POSTGRES_USER = config('POSTGRES_USER')
+POSTGRES_HOST = config('POSTGRES_HOST')
+POSTGRES_PASSWORD = config('POSTGRES_PASSWORD')
+POSTGRES_DATABASE = config('POSTGRES_DATABASE')
 
-parsed_redis_url = urlparse(KV_URL)
-redis_client = redis.StrictRedis(host=parsed_redis_url.hostname, 
-                                 port=parsed_redis_url.port, 
-                                 password=parsed_redis_url.password, 
-                                 db=0, 
-                                 decode_responses=True)
+connection = psycopg2.connect(
+    dbname=POSTGRES_DATABASE,
+    user=POSTGRES_USER,
+    password=POSTGRES_PASSWORD,
+    host=POSTGRES_HOST
+)
+cursor = connection.cursor()
 
 
 @app.route('/convert', methods=['POST'])
 @cross_origin()
 def convert_swagger():
-    try:
-        v2_doc = request.get_json()
-        
-        if not v2_doc:
-            return jsonify({"error": "Invalid input, expected a JSON payload"}), 400
+    v2_doc = request.get_json()
+    
+    if not v2_doc:
+        return jsonify({"error": "Invalid input, expected a JSON payload"}), 400
 
-        v2_doc_str = json.dumps(v2_doc)
-        v3_doc = Converter.main(v2_doc_str)
+    v2_doc_str = json.dumps(v2_doc)
+    v3_doc = Converter.main(v2_doc_str)
 
-        redis_client.incr('conversion_count')
+    cursor.execute("UPDATE stats SET conversion_count = conversion_count + 1 WHERE id = 1;")
+    connection.commit()
 
-        response = jsonify(v3_doc)
-        response.headers['Access-Control-Allow-Origin'] = '*'
 
-        return response
+    response = jsonify(v3_doc)
+    response.headers['Access-Control-Allow-Origin'] = '*'
 
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    return response
+
+
     
 @app.route('/stats', methods=['GET'])
 def get_conversion_stats():
-    count = redis_client.get('conversion_count')
-    return jsonify({"conversion_count": int(count) if count else 0})
+    cursor.execute("SELECT conversion_count FROM stats WHERE id = 1;")
+    count = cursor.fetchone()[0]
+    return jsonify({"conversion_count": count if count else 0})
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    try:
+        app.run(debug=True)
+    finally:
+        cursor.close()
+        connection.close()
