a655b440c22edc6f6c52841aef8bbd27e5aefc79
Jed Patterson <jedpatterson123@gmail.com>
2023-09-09 20:09:01 +0100

    Add Error Toaster

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,9 +5,9 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Swagger Converter",
+  title: "Swagger Converter - Convert Swagger 2.x to Swagger 3.0",
   description:
-    "Convert Swagger 2.0 to OpenAPI 3.0 format for free with this simple online tool. No registration required. We also have a public API that you can use to integrate this into your CI/CD pipeline.",
+    "Convert Swagger 2.0 to OpenAPI 3.0 format for free with this simple online tool. No registration required. ",
 };
 
 export default function RootLayout({
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, ChangeEvent } from "react";
+import { Toaster, toast } from "react-hot-toast";
 
 function debounce(func: Function, wait: number) {
   let timeout: NodeJS.Timeout;
@@ -30,8 +31,8 @@ export default function Home() {
         body: currentInputJSON,
       });
 
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
+      if (response.status === 500) {
+        toast.error("This JSON could not be converted.");
       }
 
       const data = await response.json();
@@ -62,20 +63,19 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col">
+      <Toaster />
       {/* Header */}
       <div className="bg-white shadow p-4">
-        <div className="font-medium text-xl text-gray-700">
-          Swagger Converter
-        </div>
+        <h1 className="font-medium text-xl text-gray-700">Swagger Converter</h1>
       </div>
 
       {/* Body */}
-      <div className="flex-grow flex p-4 space-x-4">
+      <div className="flex-grow flex flex-col md:flex-row p-4 md:space-x-4 space-y-4 md:space-y-0">
         {/* Left Side */}
-        <div className="flex-grow flex flex-col">
+        <div className="w-full md:w-1/2 flex flex-col space-y-4">
           <label className="mb-2 text-gray-700">Input (Swagger 2.0 JSON)</label>
           <textarea
-            className="flex-grow p-4 bg-white border-2 border-gray-300 rounded-lg shadow-md focus:ring focus:ring-indigo-500 focus:border-indigo-500"
+            className="h-1/2 md:h-full p-4 bg-white border-2 border-gray-300 rounded-lg shadow-md focus:ring focus:ring-indigo-500 focus:border-indigo-500"
             placeholder="Enter JSON here..."
             value={inputJSON}
             onChange={handleInputChange}
@@ -83,12 +83,12 @@ export default function Home() {
         </div>
 
         {/* Right Side */}
-        <div className="flex-grow flex flex-col">
+        <div className="w-full md:w-1/2 flex flex-col space-y-4">
           <label className="mb-2 text-gray-700">
             Output (Swagger 3.0 JSON)
           </label>
           <textarea
-            className="flex-grow p-4 bg-white border-2 border-gray-300 rounded-lg shadow-md focus:ring focus:ring-indigo-500 focus:border-indigo-500"
+            className="h-1/2 md:h-full p-4 bg-white border-2 border-gray-300 rounded-lg shadow-md focus:ring focus:ring-indigo-500 focus:border-indigo-500"
             placeholder="Converted JSON will appear here..."
             value={outputJSON}
             readOnly
